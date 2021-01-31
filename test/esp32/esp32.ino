e681ca9b214cbf4122323f5751b549cc1f4047d5
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2021-01-31 07:28:14 -0800

    Comment and cleanup esp32 sketch

--- test/esp32/esp32.ino
+++ test/esp32/esp32.ino
@@ -1,24 +1,33 @@
 #include <MPU9250_asukiaaa.h>
- 
+
 #ifdef _ESP32_HAL_I2C_H_
 #define SDA_PIN 21
 #define SCL_PIN 22
 #endif
 
 // See https://github.com/asukiaaa/MPU9250_asukiaaa
-struct IMU {
-  IMU(TwoWire* wire) : sensor_{MPU9250_ADDRESS_AD0_LOW} {
+class IMU {
+private:
+  // Sensor object exposed by the library we use under the hood */
+  MPU9250_asukiaaa sensor_{MPU9250_ADDRESS_AD0_LOW};
+  /*! Acceleration offsets buffer as [x, y, z] */
+  double acc_offset_[3];
+  /*! Acceleration values buffer as [x, y, z] */
+  double acc_[3] = {1.2, 0.0, 2.2};
+
+public:
+  IMU(TwoWire *wire) : sensor_{MPU9250_ADDRESS_AD0_LOW} {
     sensor_.setWire(wire);
     sensor_.beginAccel();
     sensor_.beginMag();
     sensor_.beginGyro();
   }
-  
-  MPU9250_asukiaaa sensor_{MPU9250_ADDRESS_AD0_LOW};
-  double acc_offset_[3];
-  double acc_[3] = {1.2, 0.0, 2.2};
 
-  double* acc() {
+  /*! \brief Read acceleration values from the sensor
+   *
+   *  \return Pointer to buffer with the values as [x, y, z]
+   */
+  double *acc() {
     sensor_.accelUpdate();
     acc_[0] = sensor_.accelX();
     acc_[1] = sensor_.accelY();
@@ -26,14 +35,19 @@ struct IMU {
     return acc_;
   }
 
+  /*! \brief Calibrate X,Y,Z acceleration at rest and aligned to the groud
+   *
+   *  X, Y acceleration should be 0.0
+   *  Z acceleration should be 1.0
+   */
   void calibrate() {
     double acum[3] = {0.0, 0.0, 0.0};
     const int iter = 100;
-    for (int i = 0 ; i < iter ; ++i) {
+    for (int i = 0; i < iter; ++i) {
       acc();
-      acum[0] += acc_[0];
-      acum[1] += acc_[1];
-      acum[2] += acc_[2];
+      acum[0] += acc_[0];       // X
+      acum[1] += acc_[1];       // Y
+      acum[2] += (acc_[2] - 1); // Z
     }
 
     acc_offset_[0] = acum[0] / iter;
@@ -41,7 +55,15 @@ struct IMU {
     acc_offset_[2] = acum[2] / iter;
   }
 
-  double* acc_adjusted() {
+  /*! \brief Get the offset adjusted accelerations
+   *
+   *  This method first reads acceleration values from the
+   *  sensor, and then simply substract the offsets calculated
+   *  during calibration
+   *
+   *  \return Pointer to buffer with adjusted accelerations as [x, y, z]
+   */
+  double *acc_adjusted() {
     acc();
     acc_[0] -= acc_offset_[0];
     acc_[1] -= acc_offset_[1];
@@ -49,32 +71,33 @@ struct IMU {
     return acc_;
   }
 
-  double* acc_offset() {
-    return acc_offset_;
-  }
+  double *acc_offset() { return acc_offset_; }
 };
 
-IMU* _sensor;
- 
-void setup() {
-  while(!Serial);
-   
-  Serial.begin(115200);
-   
-  #ifdef _ESP32_HAL_I2C_H_   
-  // for esp32
-  Wire.begin(SDA_PIN, SCL_PIN); //sda, scl
-  #else
-  Wire.begin();
-  #endif
+// Arduino Forces us to declare variables and objects that need to be visible
+// accross setup/loop as globals
+IMU *sensor;
 
-  _sensor = new IMU{&Wire};
-  _sensor->calibrate();
-  //Serial.println("Finished Calibrating");
+void setup() {
+  // Initialize Serial communication
+  while (!Serial)
+    ;
+  Serial.begin(115200);
+
+// Code works with ESP32 and Arduino
+#ifdef _ESP32_HAL_I2C_H_
+  // for esp32
+  Wire.begin(SDA_PIN, SCL_PIN); // sda, scl
+#else
+  Wire.begin();
+#endif
+
+  sensor = new IMU{&Wire};
+  sensor->calibrate();
 }
- 
+
 void loop() {
-  auto acc = _sensor->acc_adjusted();
+  auto acc = sensor->acc_adjusted();
   Serial.println(String(acc[0]) + " " + String(acc[1]) + " " + String(acc[2]));
   delay(50);
 }
